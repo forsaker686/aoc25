b87e3939bff84dbeb062884b562bb1bb17e35ab1
forsaker686 <95349429+forsaker686@users.noreply.github.com>
2025-12-01 10:34:13 +0100

    Refactor logic for processing input directions to get right solution for pt2

--- day1.php
+++ day1.php
@@ -5,34 +5,29 @@ fclose($vnos);
 $podatki = trim($podatki);
 $vrstice = preg_split("/\n/", $podatki);
 $start = 50;
-$stevec = 0;
-$stevec2 = 0;
+$stevec = $stevec2 = 0;  
+
 foreach($vrstice as $vrstica) {
-	$posebej = 0;
-	$smer = substr($vrstica, 0,1);
-	$vrednost = substr($vrstica,1);
-	$pVrednost = $start;
-	if($smer == 'L') {
-			$start -= $vrednost;
-		while($start < 0) {
-			$stevec2++;
-			$start += 100;
-		}
-	}else {
-		$start += $vrednost;
-		while($start > 99) {
-			$stevec2++;
-			$start -= 100;
-		}
-		var_dump($stevec2);
-	}
-	$pVrednost = $start;
-	$stevec2+= $posebej;
-	if($start == 0) {
-		$stevec++;
-	}
+    $smer = substr($vrstica, 0, 1);
+    $vrednost = intval(substr($vrstica, 1));
+
+    for($i = 0; $i < $vrednost; $i++) {
+        if ($smer == 'L') {
+            $start--;
+            if ($start < 0) $start = 99;
+        } else {
+            $start++;
+            if ($start > 99) $start = 0;
+        }
+        if ($start == 0) {
+            $stevec2++;
+        }
+    }
+    if ($start == 0) {
+        $stevec++;
+    }
 }
-echo 'part 1: '.$stevec;
-echo '<br/>';
-echo 'part 2:'.$stevec2; //works for sample, too high for real input
+
+echo "part 1: ".$stevec."<br>";
+echo "part 2: ".$stevec2;
 ?>
