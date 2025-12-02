7dfc9ab9000ddfa0370a81d175f2623772af28e0
unknown <forsaker686@modana.org>
2025-12-02 19:46:35 +0100

    added part two

--- day2.php
+++ day2.php
@@ -4,12 +4,15 @@ $podatki = fread($vnos,filesize("day2.txt"));
 fclose($vnos);
 $podatki = trim($podatki);
 $ids = preg_split("/,/", $podatki);
-$numbers = [];
+$numbers = $numbers2 =  [];
 foreach($ids as $id) {
 	[$x, $y] = explode('-', $id);
 	for($i = $x; $i <= $y; $i++) {
 		$st = strval($i);
 		if(strlen($st) >=2) {
+		    if (preg_match('/^(\d+)\1+$/', $st)) {
+	            $numbers2[] = $i;
+	        }
 			$half = ceil(strlen($st)/2);
 			if($half % 2 == 0 || $half % 3 == 0 || $half % 4 == 0 || $half % 5 == 0 || $half % 6 == 0) {
 				if(substr($st, 0, $half) == substr($st, $half)) {
@@ -25,3 +28,4 @@ foreach($ids as $id) {
 	}
 }
 echo 'part 1:'.(array_sum($numbers));
+echo 'part 2:'.(array_sum($numbers2));
