5c230a1969bcc4cca3e0da84dac20c20b470d4cc
forsaker686 <95349429+forsaker686@users.noreply.github.com>
2025-12-04 10:28:12 +0100

    added solution for part 2
    Refactor movement logic and add visualization for grid.

--- day4.php
+++ day4.php
@@ -27,7 +27,7 @@ function premikanje($y, $x, $vrstice) {
 		}
 	}
 	//desno
-	if($x+1 < $maxX) {
+	if($x+1 < $maxX -1) {
 		if($vrstice[$y][$x+1] == "@") {
 			$np++;
 		}
@@ -51,7 +51,7 @@ function premikanje($y, $x, $vrstice) {
 		}
 	}
 	//diagonala desno dol
-	if($y+1 < $maxY && $x+1 < $maxX) {
+	if($y+1 < $maxY && $x+1 < $maxX -1) {
 		if($vrstice[$y+1][$x+1] == "@") {
 			$np++;
 		}
@@ -59,7 +59,30 @@ function premikanje($y, $x, $vrstice) {
 
 	return ($np < 4 ) ? true : false;
 }
-$skupaj = 0;
+function menjaj($vrstice) {
+	$kopija = $vrstice;
+	$skupaj = 0;
+	for($i =0; $i < count($vrstice); $i++) {
+		for($j=0; $j < strlen($vrstice[$i]); $j++) {
+			if($vrstice[$i][$j] == "@") {
+				if(premikanje($i, $j, $vrstice)){
+					$kopija[$i][$j] = 'X';
+				}
+			}
+		}
+	}
+	for($i =0; $i < count($kopija); $i++) {
+		for($j=0; $j < strlen($kopija[$i]); $j++) {
+			if($kopija[$i][$j] == "X") {
+				$skupaj++;
+				$kopija[$i][$j] = '.';
+			}
+		}
+	}
+	return array('grid' => $kopija, 'sum' => $skupaj);
+}
+$skupaj =  $skupaj2 = 0;
+//part 1
 $kopija = $vrstice;
 for($i =0; $i < count($vrstice); $i++) {
 	for($j=0; $j < strlen($vrstice[$i]); $j++) {
@@ -74,7 +97,27 @@ for($i =0; $i < count($kopija); $i++) {
 	for($j=0; $j < strlen($kopija[$i]); $j++) {
 		if($kopija[$i][$j] == "X") {
 			$skupaj++;
+			$kopija[$i][$j] = '.';
 		}
 	}
 }
-echo 'part 1: '.$skupaj;
+
+//part 2
+while(true) {
+	$grid = menjaj($vrstice);
+	if($grid["sum"] == 0) {
+		break;
+	}
+	$vrstice = $grid["grid"];
+	$skupaj2 += $grid["sum"];
+}
+echo 'part 1:'.$skupaj;
+echo 'part 2:'.$skupaj2;
+
+//VISUALIZATION
+$mreza = implode('', $vrstice);
+echo '<pre>';
+echo $mreza;
+echo '</pre>';
+
+?>
