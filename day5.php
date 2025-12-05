a5e491cadc80ce676064f163d15c8f6f70c6c824
forsaker686 <95349429+forsaker686@users.noreply.github.com>
2025-12-05 09:22:50 +0100

    added part 2 logic for day5 solution
    Added logic for part 2 to calculate ranges and totals.

--- day5.php
+++ day5.php
@@ -11,14 +11,32 @@ foreach($vrstice as $vrstica) {
 	$ranges[] = [intval($start), intval($end)];
 }
 $dVrstice = preg_split("/\n/", $data);
-$skupaj = 0;
+$skupaj = $skupaj2 = 0;
+$validRange = [];
 foreach($dVrstice as $vrstica) {
 	foreach($ranges as [$start, $end]) {
 		if(intval($vrstica) >= $start && intval($vrstica) <= $end) {
 			$skupaj++;
+			$validRange[] = [$start, $end];
 			break;
 		}
 	}
 }
-
+//part 2
+usort($ranges, fn($a,$b) => $a[0] <=> $b[0]);
+$curr_s = $ranges[0][0];
+$curr_e = $ranges[0][1];
+for($i=1; $i < count($ranges); $i++) {
+	$start = $ranges[$i][0];
+	$end = $ranges[$i][1];
+	if($start <= $curr_e) {
+		$curr_e = max($curr_e, $end);
+	}else {
+		$skupaj2+= ($curr_e - $curr_s +1);
+		$curr_s = $start;
+		$curr_e = $end;
+	}
+}
+$skupaj2+= ($curr_e - $curr_s +1);
 echo 'part 1: '.$skupaj;
+echo 'part 2: '.$skupaj2;
