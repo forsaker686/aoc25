95e085e8430f67cdb357d0d4a6f790f4f5b8859e
forsaker686 <95349429+forsaker686@users.noreply.github.com>
2025-12-11 11:42:12 +0100

    added part 2 logic; Logic works on example but it's not suitable for real input

--- day11.php
+++ day11.php
@@ -6,7 +6,7 @@ $podatki = trim($podatki);
 $vrstice = preg_split("/\n/", $podatki);
 $skupaj = 0;
 $poti = [];
-$rezultati = [];
+$rezultati = $rezultati2 [];
 function isci($device, $pot, $graf) {
 	global $rezultati;
 	foreach($graf[$device] as $output) {
@@ -29,3 +29,27 @@ foreach($vrstice as $vrstica) {
 }
 isci('you', ['you'], $poti);
 echo 'part 1:'.count($rezultati);
+
+//part 2 - wokrs on example, too much time for real input
+function isci2($device, $pot, $graf, $videlDac = false, $videlFft = false) {
+	global $rezultati2;
+    if ($device === 'dac') $videlDac = true;
+    if ($device === 'fft') $videlFft = true;
+	foreach($graf[$device] as $output) {
+		if($output == 'out') {
+            if ($videlDac && $videlFft) {
+                $rezultati2[] = array_merge($pot, ['out']);
+            }
+            continue;
+		}
+		if(in_array($output, $pot)) {
+			continue;
+		}
+			$pot[] = $output;
+			isci2($output, $pot, $graf, $videlDac, $videlFft);
+			array_pop($pot);
+	}
+}
+isci2('svr', ['svr'], $poti);
+
+echo 'part 2:'.count($rezultati2);
